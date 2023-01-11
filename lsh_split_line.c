f1af53b9b180403623a76a0847c53ee52b847af3
Muhammed yasir Keles <mukeles@k2m05s07.42kocaeli.com.tr>
2023-01-11 12:15:00 +0300

    lesks

--- lsh_split_line.c
+++ lsh_split_line.c
@@ -6,7 +6,7 @@
 /*   By: mukeles <mukeles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 10:59:46 by mukeles           #+#    #+#             */
-/*   Updated: 2023/01/10 18:11:50 by mukeles          ###   ########.fr       */
+/*   Updated: 2023/01/10 18:17:50 by mukeles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ char	**lsh_split_line(char *line)
 	int		size;
 	char	**arr;
 	char *tmp;
+	char *tmp2;
 	tmp = line;
-	
+	tmp2=line;
 	size = get_cmd_num(tmp);
 	arr = malloc(sizeof(char *) * (size));
 	if (!arr)
@@ -35,7 +36,8 @@ char	**lsh_split_line(char *line)
 		arr[j] = command(tmp, &i, arr[j - 1], j);
 		system("leaks minishell > leaks4.txt");
 		j++;
-		while (tmp[i] == ' ' && tmp[i] != '\0')
+		free(tmp);
+		while (tmp2[i] == ' ' && tmp2[i] != '\0')
 			i++;
 	}
 	arr[j] = NULL;
